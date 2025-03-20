10f56054b0f2a82505a6f2873736f74d0894098f
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2025-03-20 23:28:47 +0100

    Init data

---
+++ aife/weapons/target_laser/sound/sound.bat
@@ -0,0 +1,17 @@
+tool sounds-single-layer "aife\weapons\target_laser\sound\spartan_laser_zoom_in" game_event opus
+tool sounds-single-layer "aife\weapons\target_laser\sound\spartan_laser_zoom_out" game_event opus
+
+tool sounds-single-layer "aife\weapons\target_laser\sound\tracer_by" projectile_flyby opus
+
+tool sound-looping "aife\weapons\target_laser\sound\artilary_proj_loop" projectile_flyby opus
+tool sound-looping "aife\weapons\target_laser\sound\targetlocator_fire_loop" weapon_charge opus
+
+tool sounds-single-layer "aife\weapons\target_laser\sound\target_locator_exp" projectile_detonation opus
+tool sounds-single-layer "aife\weapons\target_laser\sound\target_locator_lod" projectile_detonation opus
+
+tool sounds-single-layer "aife\weapons\target_laser\sound\targetlocator_fire" weapon_fire opus
+
+tool sounds-single-layer "aife\weapons\target_laser\sound\targetlocator_fp\target_locator_melee" weapon_melee opus
+tool sounds-single-layer "aife\weapons\target_laser\sound\targetlocator_fp\target_locator_pose1" weapon_idle opus
+tool sounds-single-layer "aife\weapons\target_laser\sound\targetlocator_fp\target_locator_pose2" weapon_idle opus
+tool sounds-single-layer "aife\weapons\target_laser\sound\targetlocator_fp\target_locator_ready" weapon_ready opus
